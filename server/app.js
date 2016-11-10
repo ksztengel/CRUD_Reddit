6ea6e7bdfeeb489ac1c97e6e57082d481ce60e65
Kristin Sztengel <kcsztengel@gmail.com>
2016-11-10 14:20:10 -0700

    ready for heroku

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-
 var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
@@ -12,13 +14,24 @@ var userapi = require('./routes/userapi');
 
 var app = express();
 
+switch (app.get('env')) {
+    case 'development':
+        app.use(morgan('dev'));
+        break;
+
+    case 'production':
+        app.use(morgan('short'));
+        break;
+
+    default:
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -59,5 +72,14 @@ app.use(function(err, req, res, next) {
   });
 });
 
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    if (app.get('env') !== 'test') {
+        // eslint-disable-next-line no-console
+        console.log('Listening on port', port);
+    }
+});
 
 module.exports = app;
--- server/knexfile.js
+++ server/knexfile.js
@@ -4,7 +4,22 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/reddit'
-  }
-
-};
+    connection: 'postgres://localhost/reddit',
+    migrations: {
+        directory: `./server/dbmigrations`,
+      },
+      seeds: {
+        directory: `./server/db/seeds`,
+      },
+    },
+    production: {
+      client: `pg`,
+      connection: `${process.env.DATABASE_URL}`,
+      migrations: {
+        directory: `./server/db/migrations`,
+      },
+      seeds: {
+        directory: `./server/db/seeds`,
+      },
+    },
+  };
